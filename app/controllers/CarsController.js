490cf4800842b3522993267eafb61a189aaebf5f
Jeremy Fowler <jembyfowler@gmail.com>
2024-02-13 14:56:07 -0700

    added notes

--- app/controllers/CarsController.js
+++ app/controllers/CarsController.js
@@ -5,6 +5,7 @@ import { Pop } from "../utils/Pop.js";
 import { setHTML } from "../utils/Writer.js";
 
 function _drawCars() {
+  // REVIEW not much has changed here
   const cars = AppState.cars
   let htmlString = ''
   cars.forEach(car => htmlString += car.CardHTMLTemplate)
@@ -12,31 +13,39 @@ function _drawCars() {
 }
 
 function _drawCarForm() {
+  // NOTE if we are not logged in
   if (!AppState.account) {
     return
   }
 
   const carFormElement = document.getElementById('carForm')
 
+  // NOTE make sure getById found an element
   if (!carFormElement) {
     return
   }
 
+  // NOTE removes d-none from our classlist, so our form renders
   carFormElement.classList.remove('d-none')
 }
 
 
 export class CarsController {
   constructor () {
+    // ANCHOR page load
     console.log('Cars controller loaded');
     this.getCars()
 
+    // ANCHOR listeners
     AppState.on('cars', _drawCars)
-    AppState.on('account', _drawCars)
-    AppState.on('account', _drawCarForm)
+
+    // NOTE we can watch the account in the AppState. When someone logs in, the account gets set which will trigger these listeners
+    AppState.on('account', _drawCars) // when someone logs in, see if we need to show delete buttons
+    AppState.on('account', _drawCarForm) // when someone logs in, allow them to create cars
   }
 
 
+  // REVIEW GET - READ
   async getCars() {
     try {
       await carsService.getCars()
@@ -47,31 +56,37 @@ export class CarsController {
     }
   }
 
+  // REVIEW POST - CREATE
   async createCar() {
     try {
-      event.preventDefault()
+      // REVIEW not much has changed here
+
+      event.preventDefault() // Don't refresh
+
       console.log('creating car');
 
-      const form = event.target
+      const form = event.target // grab HTML form
+
       console.log('car form', form);
 
-      const carFormData = getFormData(form)
+      const carFormData = getFormData(form) // pull named input values out of HTML form
 
-      // TODO where is my engine type
       console.log('object from form', carFormData);
 
-      await carsService.createCar(carFormData)
+      await carsService.createCar(carFormData) // pass data from form to service
 
       // @ts-ignore
-      form.reset()
+      form.reset() // reset form
     } catch (error) {
       console.error(error)
       Pop.error(error)
     }
   }
 
+  // REVIEW DELETE - DELETE
   async removeCar(carId) {
     try {
+      // REVIEW not much has changed here
       console.log('removing car', carId);
 
       const wantsToRemove = await Pop.confirm('Are you sure you want to delete this car for forever and ever?')
@@ -80,6 +95,7 @@ export class CarsController {
         return
       }
 
+      // NOTE the service will need the api supplied id for the car that we want to delete. Make sure your model saves that value and passes it here from the onclick
       await carsService.removeCar(carId)
 
       Pop.success('Car was deleted')
--- app/services/CarsService.js
+++ app/services/CarsService.js
@@ -3,49 +3,64 @@ import { Car } from "../models/Car.js";
 import { api } from "./AxiosService.js";
 
 class CarsService {
-  async removeCar(carId) {
-    const response = await api.delete(`api/cars/${carId}`)
-    console.log('📡 deleting car', response.data);
-
-    const carIndex = AppState.cars.findIndex(car => car.id == carId)
-
-    if (carIndex == -1) {
-      throw new Error('Index was -1, you messed up developer')
-    }
-
-    AppState.cars.splice(carIndex, 1)
-  }
-  async createCar(carFormData) {
-    // console.log('car form data', carFormData);
-
-    const response = await api.post('api/cars', carFormData)
-
-    console.log('📡 creating car', response.data);
-
-    const newCar = new Car(response.data)
-
-    console.log('new car', newCar);
-
-    AppState.cars.push(newCar)
-    // AppState.cars.unshift(newCar)
-  }
 
   async getCars() {
-    console.log('getting cars!');
 
+    // NOTE appends 'api/cars' to the end of our baseURL
     const response = await api.get('api/cars')
 
+    // NOTE always log your response data and look at it in your console
     console.log('📡 got cars', response.data);
 
+    // NOTE response.data was the array of data, so we map over that. Yesterday it was stored in an object under data (response.data.data), so always look at your response to figure out where the data is stored
     const newCars = response.data.map(carPOJO => new Car(carPOJO))
 
     console.log('mapped over cars', newCars);
 
+    // REVIEW reverses an array, so you can see newly created first
     // newCars.reverse()
 
     AppState.cars = newCars
   }
 
+  async createCar(carFormData) {
+    // console.log('car form data', carFormData);
+
+    // NOTE the second argument passed to the axios instance here becomes our request body (payload). This is the object that we want to store in the database that the codeworks api interacts with
+    const response = await api.post('api/cars', carFormData)
+
+    // NOTE the response data is the single newly created object stored in the database. Has additional fields assigned by the api such as id, _id, createdAt, updatedAt, creatorId, creator, etc...
+    console.log('📡 creating car', response.data);
+
+    // NOTE we take the single object sent back by the api and convert it into our class model
+    const newCar = new Car(response.data)
+
+    console.log('new car', newCar);
+
+    // NOTE store in our AppState, triggers the listener so user does not have to refresh to see newly created data
+    AppState.cars.push(newCar)
+
+    // REVIEW instead of adding an element to the end of an array, adds it to the beginning
+    // AppState.cars.unshift(newCar)
+  }
+
+  async removeCar(carId) {
+    // NOTE on the delete request, we specify the id of the resource that we want to delete in the request url, following the resource type. No second argument is passed, delete requests should not have a request body
+    const response = await api.delete(`api/cars/${carId}`)
+
+    // NOTE the response.data in this case is just a string telling us the resource was successfully deleted. Good to log, but we won't use it on our code after this
+    console.log('📡 deleting car', response.data);
+
+    // NOTE the car that we want to delete is in our AppState still, so we use the id from the network request to find the index of that car and splice it out
+    const carIndex = AppState.cars.findIndex(car => car.id == carId)
+
+    if (carIndex == -1) {
+      throw new Error('Index was -1, you messed up the findIndex')
+    }
+
+    // NOTE make sure we do this after the delete request was successful
+    AppState.cars.splice(carIndex, 1)
+  }
 
 }
 
