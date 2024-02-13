18de606a30d80bb90915f32e5f554c5db9f3ed4a
Seth Kunzler <sethkunzler@gmail.com>
2024-02-13 15:49:21 -0700

    completed function draw houses in controller with a listener attached to the houses array in the appstate

--- app/controllers/HousesController.js
+++ app/controllers/HousesController.js
@@ -1,5 +1,19 @@
+import { AppState } from "../AppState.js"
+import { setHTML } from "../utils/Writer.js"
+
+function _drawHouses() {
+  const houses = AppState.houses
+  let htmlString = ''
+  houses.forEach(house => htmlString += house.HouseCardTemplate)
+  setHTML('houseListings', htmlString)
+}
+
 export class HousesController {
   constructor() {
     console.log("loading houses Controller")
+
+    AppState.on('houses', _drawHouses)
   }
+
+
 }
